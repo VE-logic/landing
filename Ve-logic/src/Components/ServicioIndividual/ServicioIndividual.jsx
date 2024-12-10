b653a12db127c137445939a776da8e8ed8049464
Vane-SDeV <vanesa.soria.p@gmail.com>
2024-12-10 10:37:17 -0300

    servicios

---
+++ Ve-logic/src/Components/ServicioIndividual/ServicioIndividual.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import './ServicioIndividual.css'
+
+
+const ServicioIndividual = ({titulo, subtitulo, imagen, descripcion}) => {
+    return(
+        <div className="servicio-card">
+            <img className="servicio-imagen" src={imagen}></img>
+            <h2>{titulo}</h2>
+            <h3>{subtitulo}</h3>
+            <p>{descripcion}</p>
+        </div>
+    )
+}
+
+ServicioIndividual.propTypes = {
+    titulo: PropTypes.string.isRequired,
+    subtitulo: PropTypes.string.isRequired,
+    imagen: PropTypes.string.isRequired,
+    descripcion: PropTypes.string.isRequired,
+}
+export default ServicioIndividual;
--- Ve-logic/src/Pages/Servicios/Servicios.jsx
+++ Ve-logic/src/Pages/Servicios/Servicios.jsx
@@ -1,5 +1,48 @@
-export const Servicios = () => {
+import ServicioIndividual from './../../Components/ServicioIndividual/ServicioIndividual';
+import './Servicios.css'
+import imagen from '../../assets/imagenPrueba.png'
+
+
+const Servicios =() => {
+  const servicio =[
+    {
+      id:1,
+      titulo: 'Diseño Web',
+      subtitulo: 'Diseño',
+      descripcion: 'Desarrollamos sitios web profesionales y e-commerce, con diseño responsive y optimización para móviles.',
+      imagen: imagen,
+    },
+    {
+      id:2,
+      titulo: 'Diseño UI/UX',
+      subtitulo: 'Diseño',
+      descripcion: 'Diseñamos interfaces de usuario (UI/UX) para aplicaciones móviles y web, siguiendo los estándares de la industria.',
+      imagen: 'fas fa-desktop',
+    },
+    {
+      id:3,
+      titulo: 'Marketing Digital',
+      subtitulo: 'Diseño',
+      descripcion: 'Implementamos campañas de marketing digital, enfocadas en redes sociales, email marketing y SEO.',
+      imagen: 'fab fa-google',
+    },
+
+  ];
   return (
-    <div>Servicios</div>
-  )
-}
+    <section className="servicios">
+      <h2>Nuestros Servicios</h2>
+      <div className="servicios-grid">
+        {servicio.map(servicio => (
+          <ServicioIndividual 
+          key={servicio.id} 
+          titulo={servicio.titulo}
+          subtitulo={servicio.subtitulo}
+          descripcion={servicio.descripcion}
+          imagen={servicio.imagen}/>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Servicios;
